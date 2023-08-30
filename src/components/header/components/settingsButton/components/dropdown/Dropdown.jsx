5181da05e48b081b5db524eacdd2cc2199b932ee
Swotwor <swotwor@gmail.com>
2023-08-30 17:43:57 +0300

    fix 1

--- src/components/header/components/settingsButton/components/dropdown/Dropdown.jsx
+++ src/components/header/components/settingsButton/components/dropdown/Dropdown.jsx
@@ -1,7 +1,7 @@
 import style from './index.module.scss';
-const VITE_REQUEST_ADDRESS_IMGUR = import.meta.env.VITE_REQUEST_ADDRESS_IMGUR;
 
 const Dropdown = () => {
+    const VITE_REQUEST_ADDRESS_IMGUR = import.meta.env.VITE_REQUEST_ADDRESS_IMGUR;
     const isAuth = document.cookie.split(';')[0].split('=')[1];
     const handleClickOnButton = () => {
         document.cookie = 'access_token' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';

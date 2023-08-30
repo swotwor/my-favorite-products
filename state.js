6f6b16b7dea6ad2dc518f65e7ad44540a7f639a9
Swotwor <swotwor@gmail.com>
2023-08-30 22:17:05 +0300

    Deploy data

--- state.js
+++ state.js
@@ -1,7 +1,7 @@
 // Продукт
-//export const REQUEST_ADDRESS_MOCAPI = 'https://64ef3e20219b3e2873c42f34.mockapi.io/products/'
-//export const REQUEST_ADDRESS_IMGUR = 'https://api.imgur.com/oauth2/authorize?client_id=372bbf6ec49243d&response_type=token'
+export const REQUEST_ADDRESS_MOCAPI = 'https://64ef3e20219b3e2873c42f34.mockapi.io/products/'
+export const REQUEST_ADDRESS_IMGUR = 'https://api.imgur.com/oauth2/authorize?client_id=372bbf6ec49243d&response_type=token'
 
 // Разработка
-export const REQUEST_ADDRESS_MOCAPI = 'https://61ed9b4c634f2f00170cec9d.mockapi.io/products/';
-export const REQUEST_ADDRESS_IMGUR = 'https://api.imgur.com/oauth2/authorize?client_id=b3da1d0e7e463c1&response_type=token';
+// export const REQUEST_ADDRESS_MOCAPI = 'https://61ed9b4c634f2f00170cec9d.mockapi.io/products/';
+// export const REQUEST_ADDRESS_IMGUR = 'https://api.imgur.com/oauth2/authorize?client_id=b3da1d0e7e463c1&response_type=token';

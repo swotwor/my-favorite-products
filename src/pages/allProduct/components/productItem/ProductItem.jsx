bc65c67930d807ebba236a9198979a289e808865
Swotwor <swotwor@gmail.com>
2023-10-23 10:48:09 +0300

    Only product categories that have at least one product are displayed.

--- src/pages/allProduct/components/productItem/ProductItem.jsx
+++ src/pages/allProduct/components/productItem/ProductItem.jsx
@@ -21,19 +21,20 @@ const ProductItem = ({ item, products, openCategory, setOpenCategory }) => {
         }
     }
 
+    if (!countInCategories(products, item.id)) {
+        return false
+    }
+
     return (
         <div className={style.productItem}>
-            <p
-                onClick={() => handleClickOpenSpoyler(item.id)}
-                className={style.productItem_title}
-            >
-                <a href="#servicios">{item.title} {countInCategories(products, item.id)}</a>
+            <p onClick={() => handleClickOpenSpoyler(item.id)} className={style.productItem_title} >
+                {item.title} {countInCategories(products, item.id)}
             </p>
             {
                 openCategory == item.id
                     ? products.map(productItem => {
                             if (productItem.category == item.id) {
-                                    return <div className={style.productItem_box} key={productItem.category} id="servicios">
+                                    return <div className={style.productItem_box} key={productItem.category}>
                                         <Link
                                             to="/product_card"
                                             key={productItem.id}
